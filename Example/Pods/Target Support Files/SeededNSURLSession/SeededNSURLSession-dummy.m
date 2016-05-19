a8255c233134d1ddaaaf6fc98670cf2bfb7df733
Michael Hayman <michael@springbox.ca>
2016-05-19 08:54:28 +0200

    Don't ignore `Example/Pods`.

---
+++ Example/Pods/Target Support Files/SeededNSURLSession/SeededNSURLSession-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SeededNSURLSession : NSObject
+@end
+@implementation PodsDummy_SeededNSURLSession
+@end
---
+++ Example/Pods/Target Support Files/SeededNSURLSession/SeededNSURLSession-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SeededNSURLSessionVersionNumber;
+FOUNDATION_EXPORT const unsigned char SeededNSURLSessionVersionString[];
+
